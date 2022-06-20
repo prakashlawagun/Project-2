f548a21bc4162fc31dbfd4a7790cb3b46f7c8413
prakashlawagun <prakashlawagun11@gmail.com>
2022-06-20 13:50:29 +0545

    Adding User Registration  and solve error in validating passwords

--- Backend/account/views.py
+++ Backend/account/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from account.models import User
+from rest_framework.response import Response
+from account.serializers import UserRegistrationSerializer
+from rest_framework import status
+from rest_framework.views import APIView
+
 
 # Create your views here.
+class UserRegistrationView(APIView):
+    def post(self, request,format=None):
+        serializer = UserRegistrationSerializer(data=request.data)
+        if serializer.is_valid(raise_exception=True):
+            user = serializer.save()
+            return Response({'msg':'Registration Successful'},status=status.HTTP_201_CREATED)
+        return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
